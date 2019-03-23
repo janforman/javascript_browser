b84fb5222505f8921de4f0bd21a0e42c9e0bc2a0
Jan Forman <info@janforman.com>
2019-03-23 12:27:57 +0100

    init

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php require('password_protect.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">	
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">	
+    <title>Javascript Browser</title>	
+    <link rel="stylesheet" href="assets/css/styles.css" />  
+    <link rel="stylesheet" href="assets/fancybox/jquery.fancybox.min.css" />	
+    <script src="assets/js/jquery-1.10.0.min.js"></script> 	
+    <script src="assets/js/script.js" charset="UTF-8"></script>   
+    <script src="assets/fancybox/jquery.fancybox.min.js" charset="UTF-8"></script> 
+  </head>
+  <body>	
+    <div class="filemanager">		
+      <div class="search">			
+        <input type="search" placeholder="Find a file.." />		
+      </div>		
+      <div class="breadcrumbs">
+      </div>
+      <ul class="data">
+      </ul>		
+      <div class="nothingfound">			
+        <div class="nofiles">
+        </div>			
+        <span>No files here.
+        </span>		
+      </div>	
+    </div>
+    <script type="text/javascript">
+    $.fancybox.defaults.hash = false;
+    </script>
+  </body>
+</html>
---
+++ password_protect.php
@@ -0,0 +1,126 @@
+<?php
+##################################################################
+#  SETTINGS START
+##################################################################
+
+// Add login/password pairs below, like described above
+// NOTE: all rows except last must have comma "," at the end of line
+$LOGIN_INFORMATION = array(
+  'admin' => 'password'
+);
+
+// request login? true - show login and password boxes, false - password box only
+define('USE_USERNAME', true);
+
+// User will be redirected to this page after logout
+define('LOGOUT_URL', '');
+
+// time out after NN minutes of inactivity. Set to 0 to not timeout
+define('TIMEOUT_MINUTES', 60);
+
+// This parameter is only useful when TIMEOUT_MINUTES is not zero
+// true - timeout time from last activity, false - timeout time from login
+define('TIMEOUT_CHECK_ACTIVITY', true);
+
+##################################################################
+#  SETTINGS END
+##################################################################
+
+
+///////////////////////////////////////////////////////
+// do not change code below
+///////////////////////////////////////////////////////
+
+// timeout in seconds
+$timeout = (TIMEOUT_MINUTES == 0 ? 0 : time() + TIMEOUT_MINUTES * 60);
+
+// logout?
+if(isset($_GET['logout'])) {
+  setcookie("verify", '', $timeout, '/'); // clear password;
+  header('Location: ' . LOGOUT_URL);
+  exit();
+}
+
+if(!function_exists('showLoginPasswordProtect')) {
+
+// show login form
+function showLoginPasswordProtect($error_msg) {
+?>
+<html>
+<head>
+  <title>Please enter password to access this page</title>
+  <meta http-equiv="cache-control" content="no-cache">
+  <meta http-equiv="pragma" content="no-cache">
+  <link rel="stylesheet" href="assets/css/styles.css" /> 
+</head>
+<body>
+  <style>
+    input { border: 3px solid #373743;margin:8px; padding: 4px;border-radius: 6px }
+  </style>
+  <div class='password'>
+  <form method="post">
+    <h3>Please enter password to access this page</h3>
+    <font color="black"><?php echo $error_msg; ?></font><br />
+<?php if (USE_USERNAME) echo 'Login:<br /><input type="input" name="access_login" /><br />Password:<br />'; ?>
+    <input type="password" name="access_password" /><p></p><input type="submit" name="Submit" value="Submit" />
+  </form>
+  </div>
+</body>
+</html>
+
+<?php
+  // stop at this point
+  die();
+}
+}
+
+// user provided password
+if (isset($_POST['access_password'])) {
+
+  $login = isset($_POST['access_login']) ? $_POST['access_login'] : '';
+  $pass = $_POST['access_password'];
+  if (!USE_USERNAME && !in_array($pass, $LOGIN_INFORMATION)
+  || (USE_USERNAME && ( !array_key_exists($login, $LOGIN_INFORMATION) || $LOGIN_INFORMATION[$login] != $pass ) ) 
+  ) {
+    showLoginPasswordProtect("Incorrect password.");
+  }
+  else {
+    // set cookie if password was validated
+    setcookie("verify", md5($login.'%'.$pass), $timeout, '/');
+    
+    // Some programs (like Form1 Bilder) check $_POST array to see if parameters passed
+    // So need to clear password protector variables
+    unset($_POST['access_login']);
+    unset($_POST['access_password']);
+    unset($_POST['Submit']);
+  }
+
+}
+
+else {
+
+  // check if password cookie is set
+  if (!isset($_COOKIE['verify'])) {
+    showLoginPasswordProtect("");
+  }
+
+  // check if cookie is good
+  $found = false;
+  foreach($LOGIN_INFORMATION as $key=>$val) {
+    $lp = (USE_USERNAME ? $key : '') .'%'.$val;
+    if ($_COOKIE['verify'] == md5($lp)) {
+      $found = true;
+      // prolong timeout
+      if (TIMEOUT_CHECK_ACTIVITY) {
+        setcookie("verify", md5($lp), $timeout, '/');
+      }
+      break;
+    }
+  }
+  if (!$found) {
+    showLoginPasswordProtect("");
+  }
+
+}
+
+?>
